eac92a3b57daaac1c1f27fbe8d77a74ff75d6aec
Willi Kappler <willi-kappler@users.noreply.github.com>
2016-06-13 00:28:18 +0200

    initial release

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+//! darwin-rs: evolutionary algorithms with Rust
+//!
+//! Written by Willi Kappler, Version 0.1 (2016.06.11)
+//!
+//! Repository: https://github.com/willi-kappler/natural_constants
+//!
+//! License: MIT
+//!
+
+// For clippy
+// #![feature(plugin)]
+//
+// #![plugin(clippy)]
