a4bf91123c8e5f4ad7d96b142b7ca3464e267023
Willi Kappler <willi.kappler@uni-tuebingen.de>
2016-10-18 15:21:25 +0200

    move absolute zero to chemistry, fix return value

--- src/chemistry.rs
+++ src/chemistry.rs
@@ -1,3 +1,4 @@
 //! Chemistry related constants
 
 pub const univ_gas_const: f64 = 8.3144598; // [J K−1 mol−1]
+pub const absolute_zero: f64 = -273.15; // [°C]
--- src/conversion.rs
+++ src/conversion.rs
@@ -1,5 +1,7 @@
 //! Data types and utility function for conversion
 
+use chemistry::absolute_zero;
+
 const scale_factor: f64 = 1000.0;
 
 pub fn to_kilo(value: f64) -> f64 {
@@ -43,25 +45,25 @@ pub fn mega_to_kilo(value: f64) -> f64 {
 }
 
 pub fn to_mili(value: f64) -> f64 {
-    value * scale_factor;
+    value * scale_factor
 }
 
 pub fn from_mili(value: f64) -> f64 {
-    value / scale_factor;
+    value / scale_factor
 }
 
 pub fn to_micro(value: f64) -> f64 {
-    value * (scale_factor * scale_factor);
+    value * (scale_factor * scale_factor)
 }
 
 pub fn from_micro(value: f64) -> f64 {
-    value / (scale_factor * scale_factor);
+    value / (scale_factor * scale_factor)
 }
 
 pub fn kelvin_to_deg_cel(value: f64) -> f64 {
-    value - 273.15;
+    value + absolute_zero
 }
 
 pub fn deg_cel_to_kelvin(value: f64) -> f64 {
-    value + 273.15;
+    value - absolute_zero
 }
