28f9d72bbb6d7c80633c698e0d78cf234e18c69c
Hrvoje Hodak <dev@tribly.de>
2017-02-02 10:17:15 +0100

    Add geosciences constants

---
+++ src/geosciences.rs
@@ -0,0 +1,8 @@
+//! Geosciences related constants
+
+// taken from http://hpiers.obspm.fr/eop-pc/models/constants.html
+
+// Earth's rotation constants
+pub const mean_angular_velocity_earth: f64 = 7.29211501; // [10^(-5)rad/s]
+pub const nominal_angular_velocity_earth: f64 = 7.2921151467064	; // [10^(-5)rad/s]
+pub const conventional_duration_mean_solar_day: f64 = 86400.0; // [s]
