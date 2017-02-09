26037e46cf74da644e1269f7ce6a91d3bd5e8769
Willi Kappler <willi.kappler@uni-tuebingen.de>
2017-02-09 14:25:16 +0100

    refactoring, add struct for periodic table

--- src/chemistry.rs
+++ src/chemistry.rs
@@ -1,4 +1,49 @@
 //! Chemistry related constants
 
-pub const univ_gas_const: f64 = 8.3144598; // [J K−1 mol−1]
 pub const absolute_zero: f64 = -273.15; // [°C]
+pub const avogadros_number: f64 = 6.024e23; // [molecules/mol]
+pub const boltzmanns_constant: f64 = 1.380e-23; // [J/K * molecule]
+pub const steffan_boltzmann_constant: f64 = 5.670e-8 ; // [W/m² * K⁴]
+pub const universal_gas_constant: f64 = 8.31434e3; // [J/kmol * K]
+pub const normal_atmospheric_pressure: f64 = 101325.0; // [N/m²]
+
+pub enum StateOfMatter { // at 0 °C and 1 atm
+    Solid,
+    Liquid,
+    Gas,
+    Unknown,
+}
+
+pub enum SubCategory {
+    AlkaliMetal,
+    AlkalineEarthMetal,
+    Lanthanide,
+    Actinide,
+    TransitionMetal,
+    PostTransitionMetal,
+    Metalloid,
+    PolyatomicNonMetal,
+    DiatomicNonMetal,
+    NobleGas,
+    Unknown,
+}
+
+
+pub struct AtomInfo {
+    name: &'static str,
+    atomic_number: u32,
+    number_of_neutrons: u32,
+    mass: f64, // https://en.wikipedia.org/wiki/Relative_atomic_mass
+    state_of_matter: StateOfMatter,
+    sub_category: SubCategory,
+    // A lot of more stuff to be added...
+}
+
+pub const atom_h: AtomInfo = AtomInfo {
+    name: "H",
+    atomic_number: 1,
+    number_of_neutrons: 0,
+    mass: 1.008,
+    state_of_matter: StateOfMatter::Gas,
+    sub_category: SubCategory::DiatomicNonMetal,
+};
--- src/engineering.rs
+++ src/engineering.rs
@@ -1,19 +1,7 @@
 //! Engineering related constants
 //
-// http://www.engineeringtoolbox.com/physical-constants-SI-d_1662.html
-// http://www.engineering.com/Library/ArticlesPage/tabid/85/ArticleID/115/Constants.aspx
 
-pub const gravitational_acceleration: f64 = 9.807; // [m/s²]
-pub const plancks_constant: f64 = 6.625e-34; // [J · s/molecule]
-pub const speed_of_light_vac: f64 = 2.998e8; // [m/s]
-pub const avogadros_number: f64 = 6.024e23; // [molecules/mol]
-pub const boltzmanns_constant: f64 = 1.380e-23; // [J/K * molecule]
-pub const steffan_boltzmann_constant: f64 = 5.670e-8 ; // [W/m² * K⁴]
-pub const universal_gas_constant: f64 = 8.31434e3; // [J/kmol * K]
-pub const normal_atmospheric_pressure: f64 = 101325.0; // [N/m²]
-pub const bohr_radius: f64 = 0.529167e-10; // [m]
-pub const electronic_charge: f64 = 1.60210e-19; // [C]
-pub const electron_rest_mass: f64 = 9.10908e-31; // [kg]
-pub const fine_structure_constant: f64 = 7.29720e-3; // []
-pub const proton_rest_mass: f64 = 1.67252e-27; // [Kg]
-pub const neutron_rest_mass: f64 = 1.67482e-27; // [Kg]
+// https://en.wikipedia.org/wiki/List_of_materials_properties
+
+// https://en.wikipedia.org/wiki/Electrical_resistivity_and_conductivity
+// https://en.wikipedia.org/wiki/Thermal_diffusivity
